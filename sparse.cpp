4a9df4fe02ca15f6b96a1a74c06074f4f9baff0f
Phenomenal-one <75874519+Waseem-Raja@users.noreply.github.com>
2021-07-01 13:08:16 +0530

    Array representation of sparse matrix

--- sparse.cpp
+++ sparse.cpp
@@ -3,44 +3,56 @@
 using namespace std;
 int main()
 {
-    int size=0;
-     int spMatrix[4][5] = {0, 0, 3, 0, 4, 0, 0, 5, 7, 0, 0, 0, 0, 0, 0, 0, 2, 6, 0, 0};
-    for (int i = 0; i < 4; i++)
+    int rowsize, colsize, element, size = 0;
+    cout << "Enter the dimensions rowsize and colsize of your matrix" << endl;
+    cin >> rowsize >> colsize;
+    int *arr = new int[rowsize * colsize]; //alloting memory at heap
+    cout << "Enter the elements" << endl;
+    for (int i = 0; i < rowsize; i++)
     {
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < colsize; j++)
         {
-            if (spMatrix[i][j] != 0)
-            {
-                size++; //number of non-zero elements
-            }
+
+            // Assign values to
+            // the memory block
+            cin >> element;
+            if (element != 0)
+                size++;
+
+            *(arr + i * colsize + j) = element;
         }
     }
-    cout<<size<<endl;
+    //cout << size << endl;
+
     // number of columns in compactMatrix (size) must be
     // equal to number of non - zero elements in
     // sparseMatrix
-    int traingulermat[3][size]; //row,column and value as three rows and size as col size
+    int compactt[3][size]; //row,column and value as three rows and size as col size
 
     // Making of new matrix
     int k = 0;
-    for (int i = 0; i < 4; i++){
-        for (int j = 0; j < 5; j++){
-            if (spMatrix[i][j] != 0)
+    for (int i = 0; i < rowsize; i++)
+    {
+        for (int j = 0; j < colsize; j++) 
+        {
+            if (*(arr + i * colsize + j) != 0)
             {
-                traingulermat[0][k] = i;              //ith index
-                traingulermat[1][k] = j;              //jth index
-                traingulermat[2][k] = spMatrix[i][j]; //i and jth index value
+                compactt[0][k] = i;                        //ith index
+                compactt[1][k] = j;                        //jth index
+                compactt[2][k] = *(arr + i * colsize + j); //i and jth index value
                 k++;
             }
         }
     }
+    delete[] arr;
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < size; j++)
         {
-            cout << traingulermat[i][j] << " ";
+            cout << compactt[i][j] << " ";
         }
         cout << endl;
     }
+
     return 0;
 }
